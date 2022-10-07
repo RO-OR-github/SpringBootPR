fac73b926b468607fe2f1b57e4fe50342a616b17
RO-OR <kyk3531@gmail.com>
2022-10-08 05:12:43 +0900

    Chapter2 PR

---
+++ src/test/java/com/practice/dyd/practicespringboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package com.practice.dyd.practicespringboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lb_func_test() {
+
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
